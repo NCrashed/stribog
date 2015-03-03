cf273c9d5b0e74d39d0787409b6489d894836693
NCrashed <NCrashed@gmail.com>
2015-03-03 04:40:14 +0400

    MultiKeyMap API refactored to be able usage with symbols in user scope.

--- source/stribog/container/multiKeyMap.d
+++ source/stribog/container/multiKeyMap.d
@@ -8,139 +8,146 @@
 */
 module stribog.container.multiKeyMap;
 
-import std.conv;
-import stribog.meta;
-
-class MultiKeyMap(KeyValuesRaw...)
+mixin template makeMultiKeyMap(string mapTypeName, KeyValuesRaw...)
 {
-    private alias KeyValues = SplitKeyValue!KeyValuesRaw;
-    
-    auto opIndex(K)(K val)
-        if(hasKeyImpl!(K, KeyValues))
-    {
-        alias KV = getPair!(K, KeyValues); 
-        mixin("return " ~ KV.mapName!() ~ "[val];");  
-    }
-    
-    void opIndexAssign(K,V)(V val, K key)
-        if(hasKeyImpl!(K, KeyValues) && is(getPair!(K, KeyValues).Value == V))
-    {
-        alias KV = getPair!(K, KeyValues); 
-        mixin(KV.mapName!() ~ "[key] = val;"); 
-    }
-    
-    bool hasKey(K)(K key) if(!hasKeyImpl!(K, KeyValues))
-    {
-        return false;
-    }
-    
-    bool hasKey(K)(K key) if(hasKeyImpl!(K, KeyValues))
-    {
-        alias KV = getPair!(K, KeyValues); 
-        mixin("return (key in " ~ KV.mapName!() ~ ") !is null;");
-    }
-    
-    //pragma(msg, GenMaps!KeyValues);
-    mixin(GenMaps!KeyValues);
-}
+    import stribog.meta;
+    import std.conv;
+    import std.traits : fullyQualifiedName;
 
-private template KeyValue(_Key, _Value, size_t _i)
-{
-    enum i = _i;
-    alias Key = _Key;
-    alias Value = _Value;
-    
-    template setNumber(size_t i) {
-        alias setNumber = KeyValue!(Key, Value, i);
-    }
-
-    template mapName() {
-        enum mapName = text("map", i);
-    }
-    
-    template genCode() {
-        enum genCode = text(Value.stringof ~ "[" ~ Key.stringof ~ "] " ~ mapName!() ~ ";\n");
-    }
-}
-
-private template SplitKeyValue(T...)
-{
-    static assert(T.length % 2 == 0, text("Key-values pairs count isn't even, got ", T.length, " elements"));
-    
-    private template makeKeyValue(K, V)
+    mixin(q{class }~mapTypeName~q{
     {
-        alias makeKeyValue = KeyValue!(K, V, 0);
-    }
-    
-    private template incNumber(alias Pair, T...)
-    {
-        enum iacc = Pair.expand[0];
-        alias kv = T[0];
-        alias kvs = Pair.expand[1];
-        alias incNumber = StrictExpressionList!(iacc+1, StrictExpressionList!(kv.setNumber!(iacc), kvs.expand));
-    }
-    
-    alias Temp = staticFold!( incNumber, StrictExpressionList!(0, StrictExpressionList!()), staticMap2!(makeKeyValue, T)).expand;
-    alias Temp2 = Temp[1];
-    alias SplitKeyValue = Temp2.expand;
-}
+        private alias KeyValues = SplitKeyValue!KeyValuesRaw;
+        
+        auto opIndex(K)(K val)
+            if(hasKeyImpl!(K, KeyValues))
+        {
+            alias KV = getPair!(K, KeyValues); 
+            mixin("return " ~ KV.mapName!() ~ "[val];");  
+        }
+        
+        void opIndexAssign(K,V)(V val, K key)
+            if(hasKeyImpl!(K, KeyValues) && is(getPair!(K, KeyValues).Value == V))
+        {
+            alias KV = getPair!(K, KeyValues); 
+            mixin(KV.mapName!() ~ "[key] = val;"); 
+        }
+        
+        bool hasKey(K)(K key) if(!hasKeyImpl!(K, KeyValues))
+        {
+            return false;
+        }
+        
+        bool hasKey(K)(K key) if(hasKeyImpl!(K, KeyValues))
+        {
+            alias KV = getPair!(K, KeyValues); 
+            mixin("return (key in " ~ KV.mapName!() ~ ") !is null;");
+        }
+        
+        //pragma(msg, GenMaps!KeyValues);
+        mixin(GenMaps!KeyValues);
+    }});
 
-private template GenMaps(T...)
-{
-    private template accString(string acc, T...)
+    private template KeyValue(_Key, _Value, size_t _i)
     {
-        enum accString = acc ~ T[0].genCode!();
-    }
+        enum i = _i;
+        alias Key = _Key;
+        alias Value = _Value;
+        
+        template setNumber(size_t i) {
+            alias setNumber = KeyValue!(Key, Value, i);
+        }
     
-    enum GenMaps = staticFold!(accString, "", T);
-}
-
-private template hasKeyImpl(U, T...)
-{
-    private template hasKeyImplImpl(bool acc, Params...)
-    {
-        static if(acc) 
-            enum hasKeyImplImpl = true;
-        else {
-            alias Param = Params[0];
-            alias Key = Param.Key;
-            enum hasKeyImplImpl = is(Key == U);
+        template mapName() {
+            enum mapName = text("map", i);
+        }
+        
+        template genCode() {
+            enum genCode = text(fullyQualifiedName!Value ~ "[" ~ fullyQualifiedName!Key ~ "] " ~ mapName!() ~ ";\n");
         }
     }
     
-    alias hasKeyImpl = staticFold!(hasKeyImplImpl, false, T);
-}
-
-private template getPair(Key, T...)
-{
-    private template getPairImpl(alias Res, Params...)
+    private template SplitKeyValue(T...)
     {
-        static if(Res.expand.length > 0) 
-            alias getPairImpl = Res;
-        else {
-            alias Param = Params[0];
-            alias LocalKey = Param.Key;
-            static if(is(LocalKey == Key)) 
-                alias getPairImpl = StrictExpressionList!(Param);
-            else
-                alias getPairImpl = StrictExpressionList!();
+        static assert(T.length % 2 == 0, text("Key-values pairs count isn't even, got ", T.length, " elements"));
+        
+        private template makeKeyValue(K, V)
+        {
+            alias makeKeyValue = KeyValue!(K, V, 0);
         }
+        
+        private template incNumber(alias Pair, T...)
+        {
+            enum iacc = Pair.expand[0];
+            alias kv = T[0];
+            alias kvs = Pair.expand[1];
+            alias incNumber = StrictExpressionList!(iacc+1, StrictExpressionList!(kv.setNumber!(iacc), kvs.expand));
+        }
+        
+        alias Temp = staticFold!( incNumber, StrictExpressionList!(0, StrictExpressionList!()), staticMap2!(makeKeyValue, T)).expand;
+        alias Temp2 = Temp[1];
+        alias SplitKeyValue = Temp2.expand;
     }
     
-    alias getPair = staticFold!(getPairImpl, StrictExpressionList!(), T).expand[0];
+    private template GenMaps(T...)
+    {
+        private template accString(string acc, T...)
+        {
+            enum accString = acc ~ T[0].genCode!();
+        }
+        
+        enum GenMaps = staticFold!(accString, "", T);
+    }
+    
+    private template hasKeyImpl(U, T...)
+    {
+        private template hasKeyImplImpl(bool acc, Params...)
+        {
+            static if(acc) 
+                enum hasKeyImplImpl = true;
+            else {
+                alias Param = Params[0];
+                alias Key = Param.Key;
+                enum hasKeyImplImpl = is(Key == U);
+            }
+        }
+        
+        alias hasKeyImpl = staticFold!(hasKeyImplImpl, false, T);
+    }
+    
+    private template getPair(Key, T...)
+    {
+        private template getPairImpl(alias Res, Params...)
+        {
+            static if(Res.expand.length > 0) 
+                alias getPairImpl = Res;
+            else {
+                alias Param = Params[0];
+                alias LocalKey = Param.Key;
+                static if(is(LocalKey == Key)) 
+                    alias getPairImpl = StrictExpressionList!(Param);
+                else
+                    alias getPairImpl = StrictExpressionList!();
+            }
+        }
+        
+        alias getPair = staticFold!(getPairImpl, StrictExpressionList!(), T).expand[0];
+    }
 }
 
-unittest
+version(unittest)
 {
     import std.range;
     
-    alias M = MultiKeyMap!(
+    mixin makeMultiKeyMap!("MultiKeyMap", 
         int, uint,
         char, ubyte,
         ulong, char[17],
-        int[42], bool);
-    
-    M map = new M();
+        int[42], bool
+    );
+}
+unittest
+{
+    MultiKeyMap map = new MultiKeyMap();
     
     map[cast(int)5] = 42u; 
     assert(map[cast(int)5] == 42u);
